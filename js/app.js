ebdbfd8e6c8311f6fa37a0fb8957fb27c78086c1
reema <reeroo02@gmail.com>
2020-11-04 21:57:10 +0300

    finished adding navbar in javascript

--- js/app.js
+++ js/app.js
@@ -27,6 +27,9 @@
 
 
 
+
+
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -34,12 +37,39 @@
 */
 
 // build the nav
+function navBuilder(){
+    // first, let me get the nav:
+    let navbar_list = document.querySelector("#navbar__list"); //  ul list
 
+    // I'll get the sections using their IDs, add them in an array: 
+    list_of_sections= [];
+    list_of_sections.push(document.getElementById("section1"));
+    list_of_sections.push(document.getElementById("section2"));
+    list_of_sections.push(document.getElementById("section3"));
+    list_of_sections.push(document.getElementById("section4"));
+    // ~ New Sections can be added here ~
+    
+    // I'll loop over the sections, get the text content based on the data-nav:
+    for(let i=0; i<list_of_sections.length; i++){
+        // Create anchor and list item:
+        anchor = document.createElement("a"); 
+        let nav_element = document.createElement("li");
+
+        // set href of anchor to the section based on its id: 
+        //-- #source for this idea:https://www.geeksforgeeks.org/how-to-create-a-link-in-javascript/ -- 
+        anchor.href ="#"+list_of_sections[i].getAttribute("id");
+
+        // get the text content based on the data-nav:
+        nav_element.textContent= list_of_sections[i].getAttribute("data-nav");
+        // append li to a 
+        anchor.appendChild(nav_element);
+        // append a to nav 
+        navbar_list.appendChild(anchor);
+    } // will use fragment if possabile.
+}
 
 // Add class 'active' to section when near top of viewport
 
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
@@ -49,7 +79,7 @@
 */
 
 // Build menu 
-
+navBuilder();
 // Scroll to section on link click
 
 // Set sections as active
